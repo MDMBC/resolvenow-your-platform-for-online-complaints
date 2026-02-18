8449a819c6457bdf4d4ef5179fae08e8ce711e95
M.Balachandar <balachandarmdmangalam@gmail.com>
2026-02-19 00:50:14 +0530

    Re-adding project

--- backend/config.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-mongoose.connect("mongodb+srv://myAtlasDBUser:Bala123@myatlasclusteredu.glscbwd.mongodb.net/details?appName=myAtlasClusterEDU")
-.then(()=>{
-   console.log("connected to mongodb atlas")
-})
-.catch((err)=>{
-   console.log(err)
-})
